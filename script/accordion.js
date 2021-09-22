1a1fc8e69948b8a2a21ef2623c6a78f564fef584
cauchzira <dfishchenko@yandex.ru>
2021-09-22 21:30:10 +0700

    Refactor accordion.js

--- script/accordion.js
+++ script/accordion.js
@@ -3,25 +3,14 @@ const lists = document.querySelectorAll(".feature-sub");
 //
 btns.forEach((btnItem, index) => {
   btnItem.addEventListener("click", () => {
-    btns.forEach((btnItem) => {
-      if (btnItem.classList.contains("feature__link_active")) {
+    btns.forEach((btnItem, id) => {
+      if (btnItem === id) {
+        btnItem.classList.toggle("feature__link_active");
+        lists[id].classList.toggle("hidden");
+      } else {
         btnItem.classList.remove("feature__link_active");
+        lists[id].classList.add("hidden");
       }
     });
-    btnItem.classList.add("feature__link_active");
-
-    lists.forEach((listItem, index) => {
-      listItem.classList.add("hidden");
-    });
-
-    lists[index].classList.remove("hidden");
-
-    //Закрытие открытого блока
-    for (let i = 0; i < btns.length; i++) {
-      btns[i].addEventListener("click", () => {
-        btns[i].classList.toggle("feature__link_active");
-        lists[i].classList.toggle("hidden");
-      });
-    }
   });
 });
