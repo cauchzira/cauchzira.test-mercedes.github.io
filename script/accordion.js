6dae67de363dd6f62ce1da8a0af4ed6551b934ed
cauchzira <dfishchenko@yandex.ru>
2021-09-22 21:02:28 +0700

    Update accordion.js

--- script/accordion.js
+++ script/accordion.js
@@ -1,32 +1,27 @@
-const btns = document.querySelectorAll('.feature__link');
-const lists = document.querySelectorAll('.feature-sub');
+const btns = document.querySelectorAll(".feature__link");
+const lists = document.querySelectorAll(".feature-sub");
 //
-btns.forEach((btnItem,index) => {
-    btnItem.addEventListener('click', () => {
-        btns.forEach((btnItem ) => {
-            if(btnItem.classList.contains('feature__link_active')) {
-                btnItem.classList.remove('feature__link_active')
-            };
+btns.forEach((btnItem, index) => {
+  btnItem.addEventListener("click", () => {
+    btns.forEach((btnItem) => {
+      if (btnItem.classList.contains("feature__link_active")) {
+        btnItem.classList.remove("feature__link_active");
+      }
+    });
+    btnItem.classList.add("feature__link_active");
 
-        })
-        btnItem.classList.add('feature__link_active')
+    lists.forEach((listItem, index) => {
+      listItem.classList.add("hidden");
+    });
 
+    lists[index].classList.remove("hidden");
 
-
-        lists.forEach((listItem, index) => {
-            listItem.classList.add('hidden');
-        })
-
-        lists[index].classList.remove('hidden');
-
-
-        for (let i = 0; i < btns.length; i++) {
-            btns[i].addEventListener('click', () => {
-                btns[i].classList.toggle('feature__link_active');
-                lists[i].classList.toggle('hidden');
-            })
-        }
-    })
-
-
-})
+    //Закрытие открытого блока
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", () => {
+        btns[i].classList.toggle("feature__link_active");
+        lists[i].classList.toggle("hidden");
+      });
+    }
+  });
+});
